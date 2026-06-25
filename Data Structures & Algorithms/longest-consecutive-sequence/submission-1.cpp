bb0c391ecc8c129e249130dc450c18ddb8673ade
Mohammad Bashar Khalaf <mohammadkhalaf3hfg@gmail.com>
2026-06-26 02:22:34 +0300

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> set;
+        vector<int> v;
+        if(nums.size() == 1)return 1;
+        else{
+        for (int num : nums) {
+            set.insert(num);
+        }
+        for (int num : set) {
+            if (set.count(num+1)||set.count(num-1)) {
+                v.push_back(num);
+            }
+        }
+        cout << "[";
+
+        for (int num : v) {
+            cout<< num << " ";
+        }
+        cout << "]";
+        cout << endl;
+        return v.size();
+        }
+}
+};
